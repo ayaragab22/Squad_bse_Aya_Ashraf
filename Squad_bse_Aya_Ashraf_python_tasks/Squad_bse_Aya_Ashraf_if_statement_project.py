9f64c6b78b13294fe7eaa97dc330827f4edd55b9
ayaragab22 <50641067+ayaragab22@users.noreply.github.com>
2022-09-20 23:16:46 +0200

    update

---
+++ Squad_bse_Aya_Ashraf_python_tasks/Squad_bse_Aya_Ashraf_if_statement_project.py
@@ -0,0 +1,11 @@
+s = int(input("Enter your score : "))
+if s >= 90:
+    print("Your grade is : A")
+elif s >= 80:
+    print("Your grade is : B")    
+elif s >= 70:
+    print("Your grade is : C")    
+elif s >= 60:
+    print("Your grade is : D")    
+else:
+    print("Your grade is : F")
